f392031d9a1928522df75cbb9d5e26df38f7c0e4
DanilProggg <krivoboc.krivoboc@gmail.com>
2023-03-30 09:11:38 +0300

    start do tags menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,43 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContentComponent } from './static_components/content/content.component';
-import { NavPanelComponent } from './static_components/nav-panel/nav-panel.component';
-import { HeaderComponent } from './static_components/header/header.component';
 import {Routes, RouterModule} from "@angular/router";
-import { CreateArticleComponent } from './components/create-article/create-article.component';
-import { HomeComponent } from './components/home/home.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ShowArticlesComponent } from './components/show-articles/show-articles.component';
-import { AnyArticleComponent } from './components/any-article/any-article.component';
-import { LoginComponent } from './components/login/login.component';
+import {HeaderComponent} from "./components/header/header.component";
 
 
-
-const appRoutes : Routes =[
-  {path:'', component: HomeComponent},
-  {path:'login', component:LoginComponent},
-  {path:'create', component: CreateArticleComponent},
-  {path:'articles', component:ShowArticlesComponent},
-  {path:'**',component:NotFoundComponent}
+const routes : Routes = [
 ]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent,
-    NavPanelComponent,
-    HeaderComponent,
-    CreateArticleComponent,
-    HomeComponent,
-    NotFoundComponent,
-    ShowArticlesComponent,
-    AnyArticleComponent,
-    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+
   ],
   providers: [],
   bootstrap: [AppComponent]
