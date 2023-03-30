22c21f7613aa1e7c8ac0558a5517e4f8fa056043
DanilProggg <krivoboc.krivoboc@gmail.com>
2023-03-30 16:51:58 +0300

    header update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styles: [
+    `.showTags{background-color:#d4d4d8;}
+     .showTheme{background-color:#d4d4d8;}`
+  ]
 })
 export class HeaderComponent {
+  showTags:boolean = false
+  showTheme:boolean = false
+   toggleTags(){
+    this.showTags=!this.showTags
+     this.showTheme = false
+   }
+   toggleTheme(){
+    this.showTheme=!this.showTheme
+     this.showTags = false
+   }
 
 }
